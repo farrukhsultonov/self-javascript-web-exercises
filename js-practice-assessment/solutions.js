ba13a380be5367443ca00c08b3b28fccabb68f73
Farrukh <farrukh.sultonov@codeup.edu>
2023-10-20 13:59:31 -0500

    finished js-practice assessment version-one

---
+++ js-practice-assessment/solutions.js
@@ -0,0 +1,90 @@
+"use strict";
+
+/**
+ * Write your solutions here.
+ *
+ * **Note**: While normally it is good practice to wrap your javascript in an
+ * immediately invoked function expression (iife), you should _not_ do that
+ * here. If you do, the automated tests will not be able to see your functions.
+ */
+
+// 1. isGreaterThan
+function isGreaterThan(input1, input2) {
+    return input1 > input2;
+}
+
+
+// 2. isLessThan
+function isLessThan(lessInput1, lessInput2){
+    return parseFloat(lessInput1) < parseFloat(lessInput2);
+}
+
+
+// 3. isEqualNumber
+function isEqualNumber(equalNumInput1, equalNumInput2){
+    return parseFloat(equalNumInput1) === parseFloat(equalNumInput2);
+}
+
+
+//  4. isEqual
+function isEqual(input1, input2) {
+    return input1 === input2
+}
+
+
+// 5. isNot
+function isNot(notInput) {
+    if (notInput === null){
+        return false
+    } else {
+        return !notInput;
+    }
+}
+
+
+// 6. calcAbs
+function calcAbs(absInput){
+    if (typeof absInput == 'string'){
+        return 0
+    } else {
+        return Math.abs(absInput);
+    }
+}
+
+
+// 7. calcCube
+function calcCube(cubeInput) {
+    if (typeof cubeInput == 'string') {
+        return 0
+    } else {
+        return cubeInput * cubeInput * cubeInput;
+    }
+}
+
+
+// 8. isTypeMatch
+function isTypeMatch(typeMatchInput1, typeMatchInput2){
+    return typeof typeMatchInput1 === typeof typeMatchInput2;
+}
+
+
+// 9. fundLongestString
+function findLongestString(longestInput1, longestInput2){
+    if (typeof longestInput1 === 'string' && typeof longestInput2 !== 'string') {
+        return longestInput1;
+    } else if(typeof longestInput2 === 'string' && typeof longestInput1 !== 'string') {
+        return longestInput2;
+    } else if(typeof longestInput2 !== 'string' && typeof longestInput1 !== 'string') {
+        return "";
+    } else if (longestInput1.length > longestInput2.length) {
+        return longestInput1;
+    } else {
+        return longestInput2;
+    }
+}
+
+
+// 10. isFactor
+function isFactor(factorInput1, factorInput2){
+    return parseFloat(factorInput2) % parseFloat(factorInput1) === 0;
+}
