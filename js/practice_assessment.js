0cc1887336d5488cef7583f275e23fab360d16ef
Farrukh <farrukh.sultonov@codeup.edu>
2023-10-24 09:16:48 -0500

    finished date objects exercise

--- js/practice_assessment.js
+++
@@ -1,9 +0,0 @@
-function isGreaterThan(value1, value2) {
-    // Check if both arguments are numbers
-    if (typeof value1 !== 'number' || typeof value2 !== 'number') {
-        return false;
-    }
-
-    // Compare the values and return the result
-    return value1 > value2;
-}
